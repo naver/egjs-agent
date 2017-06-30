5d68758390b1b2e117d74be33bb589c1a0a6b569
Heejae Kim <heejae.kim@navercorp.com>
2017-06-30 18:46:18 +0900

    Updates

--- release/2.0.0-rc2/dist/agent.js
+++ release/2.0.0-rc2/dist/agent.js
@@ -5,7 +5,7 @@
  * @egjs/agent JavaScript library
  * 
  * 
- * @version 2.0.0-pre
+ * @version 2.0.0-rc2
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -103,33 +103,53 @@ var _Parser2 = _interopRequireDefault(_Parser);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 /**
+ * @typedef {Object} BrowserInfo
+ * @property {String} name browser name
+ * @property {String} version browser version
+ * @property {Boolean} webview is webview or not
+ */
+
+/**
+ * @typedef {Object} OsInfo
+ * @property {String} name OS name
+ * @property {String} version OS version
+ */
+
+/**
+ * @typedef {Object} AgentInfo
+ * @property {BrowserInfo} browser browser information
+ * @property {OsInfo} os browser information
+ */
+
+/**
  * Extracts browser and operating system information from the user agent string.
  * @ko 유저 에이전트 문자열에서 브라우저와 운영체제 정보를 추출한다.
- * @alias eg.Agent
+ * @param {String} [userAgent=navigator.userAgent] user agent string to parse <ko>파싱할 유저에이전트 문자열</ko>
+ * @return {Object} result
  */
 /**
  * Copyright (c) NAVER Corp.
  * egjs-agent projects are licensed under the MIT license
  */
 function Agent() {
-	var ua = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : _browser.navigator.userAgent;
+  var ua = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : _browser.navigator.userAgent;
 
-	_Parser2.default.setUa(ua);
+  _Parser2.default.setUa(ua);
 
-	var agent = {
-		os: _Parser2.default.getOs(),
-		browser: _Parser2.default.getBrowser()
-	};
+  var agent = {
+    os: _Parser2.default.getOs(),
+    browser: _Parser2.default.getBrowser()
+  };
 
-	agent.browser.name = agent.browser.name.toLowerCase();
-	agent.os.name = agent.os.name.toLowerCase();
-	agent.os.version = agent.os.version.toLowerCase();
+  agent.browser.name = agent.browser.name.toLowerCase();
+  agent.os.name = agent.os.name.toLowerCase();
+  agent.os.version = agent.os.version.toLowerCase();
 
-	if (agent.os.name === "ios" && agent.browser.webview) {
-		agent.browser.version = "-1";
-	}
+  if (agent.os.name === "ios" && agent.browser.webview) {
+    agent.browser.version = "-1";
+  }
 
-	return agent;
+  return agent;
 }
 
 exports.default = Agent;
@@ -324,7 +344,7 @@ var _Agent2 = _interopRequireDefault(_Agent);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-_Agent2.default.VERSION = "2.0.0-pre"; /**
+_Agent2.default.VERSION = "2.0.0-rc2"; /**
                                         * Copyright (c) NAVER Corp.
                                         * egjs-agent projects are licensed under the MIT license
                                         */
