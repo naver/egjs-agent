5535978cc896786a8672d669e2afa85fa911900e
Heejae Kim <heejae.kim@navercorp.com>
2018-03-16 11:57:14 +0900

    chore(release): Release 2.1.3

--- dist/agent.js
+++ dist/agent.js
@@ -5,7 +5,7 @@
  * @egjs/agent JavaScript library
  * 
  * 
- * @version 2.1.3-rc
+ * @version 2.1.3
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -161,7 +161,7 @@ function agent() {
 
   return agentInfo;
 }
-agent.VERSION = "2.1.3-rc";
+agent.VERSION = "2.1.3";
 exports["default"] = agent;
 module.exports = exports["default"];
 
